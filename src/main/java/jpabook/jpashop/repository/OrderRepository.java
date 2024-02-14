823940a029084d895bd1a0cf98fc454963cd046e
Ran <lan4250@naver.com>
2024-02-15 00:30:55 +0900

    feat/ (컬렉션 조회 최적화) V3 엔티티를 DTO로 변환 - 페이징과 한계돌파 (1)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -86,8 +86,18 @@ public class OrderRepository {
 
     public List<Order> findAllWithMemberDelivery(){
         return em.createQuery("SELECT o FROM Order o  " +
+                        " JOIN FETCH o.member m" +
+                        " JOIN FETCH o.delivery d", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit){
+        return em.createQuery("SELECT o FROM Order o  " +
                 " JOIN FETCH o.member m" +
-                " JOIN FETCH o.delivery d", Order.class).getResultList();
+                " JOIN FETCH o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
     }
 
     public List<Order> findAllWithItem(){
